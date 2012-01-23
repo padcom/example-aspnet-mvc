1eb0d8f16e686e37d5ff51ef45558540e11c29b5
Matthias Hryniszak <padcom@gmail.com>
2012-01-23 22:48:02 +0100

    Added retrieving entries from ihatejsf.com MongoDB

--- Example/Controllers/HomeController.cs
+++ Example/Controllers/HomeController.cs
@@ -4,10 +4,18 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+
 namespace Example.Controllers {
     public class HomeController : Controller {
         public ActionResult Index() {
             ViewData["Message"] = "Hello, world! from ASP.NET MVC";
+
+            var server = MongoServer.Create("mongodb://heroku_app522285:r1r0ig3q3fbohld0tk9tf90ccj@ds029287.mongolab.com:29287/heroku_app522285");
+            var database = server.GetDatabase("heroku_app522285", server.Settings.DefaultCredentials);
+            ViewData["Posts"] = database.GetCollection("posts").FindAll().SetSortOrder(SortBy.Descending("created"));
+
             return View();
         }
 
--- Example/Views/Home/Index.aspx
+++ Example/Views/Home/Index.aspx
@@ -1,5 +1,6 @@
 ﻿<%@ Page Language="C#" Inherits="System.Web.Mvc.ViewPage<dynamic>" %>
-
+<%@ Import Namespace="MongoDB.Driver" %>
+<%@ Import Namespace="MongoDB.Bson" %>
 <!DOCTYPE html>
 
 <html>
@@ -8,5 +9,12 @@
 </head>
 <body>
     <h1><%= ViewData["Message"] %></h1>
+
+    <ul>
+        <% var posts = (MongoCursor<BsonDocument>)ViewData["Posts"]; %>
+        <% foreach (var post in posts) { %>
+            <li><%= post["text"] %></li>
+        <% } %>
+    </ul>
 </body>
 </html>
