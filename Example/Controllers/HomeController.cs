77cfbdcd537909ed8e39f6291355e84f2626376e
Matthias Hryniszak <padcom@gmail.com>
2012-01-23 23:13:03 +0100

    Moved from environment variables to application settings.

--- Example/Controllers/HomeController.cs
+++ Example/Controllers/HomeController.cs
@@ -6,13 +6,14 @@ using System.Web.Mvc;
 
 using MongoDB.Driver;
 using MongoDB.Driver.Builders;
+using System.Configuration;
 
 namespace Example.Controllers {
     public class HomeController : Controller {
         public ActionResult Index() {
             ViewData["Message"] = "Hello, world! from ASP.NET MVC";
 
-            var connectionString = System.Environment.GetEnvironmentVariable("MONGOLAB_URI");
+            var connectionString = ConfigurationSettings.AppSettings["MONGOLAB_URI"];
             var uri = new Uri(connectionString);
             var server = MongoServer.Create(uri);
             var database = server.GetDatabase(uri.LocalPath.Replace("/", ""), server.Settings.DefaultCredentials);
