d7a9a207ca39d58056c823310c12fbbcfead1ed7
Matthias Hryniszak <padcom@gmail.com>
2012-01-23 22:02:37 +0100

    Initial import

---
+++ Example/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Example.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            ViewData["Message"] = "Hello, world! from ASP.NET MVC";
+            return View();
+        }
+
+    }
+}
---
+++ Example/Views/Home/Index.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Language="C#" Inherits="System.Web.Mvc.ViewPage<dynamic>" %>
+
+<!DOCTYPE html>
+
+<html>
+<head runat="server">
+    <title>Example</title>
+</head>
+<body>
+    <h1><%= ViewData["Message"] %></h1>
+</body>
+</html>
